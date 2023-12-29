c6884424de5c7829da0582a500a3dcf1cd3ef586
dzhan111 <zhandavid4@gmail.com>
2023-12-28 22:08:12 -0500

    make a bunch of code

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # WordHunt-Solver
+
+
+find all combinations for letters in wordhunt
+create a trie containing all possible words given our board - DFS from each letter
+store the words in a trie
+output the list of words, sorted from longest to shortest
+
+target runtime: <1 sec
+
+params:
+- 1:20 time
+- 4x4 box
---
+++ wordhunt.py
@@ -0,0 +1,56 @@
+board = {1: '', 2: '', 3: '', 4: '',
+         5: '', 6: '', 7: '', 8: '',
+         9: '', 10: '', 11: '', 12: '',
+         13: '', 14: '', 15: '', 16: ''
+         }
+
+def getBoard():
+    user_input = input("Input board: ")
+    i = 1
+    for letter in user_input:
+        board[i] = letter
+        i = i + 1
+
+def printBoard():
+    for i in range(1, 17):
+        print(board[i], end=' ')
+        if i % 4 == 0:
+            print()
+
+
+
+#trie class
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end_of_word = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        current_node = self.root
+
+        for char in word:
+            if char not in current_node.children:
+                current_node.children[char] = TrieNode()
+            current_node = current_node.children[char]
+
+        current_node.is_end_of_word = True
+
+
+trie = Trie()
+
+with open("wordbank.txt", "r") as file:
+    # Read the first line
+    line = file.readline()
+
+    # While there are more lines in the file (line is not an empty string)
+    while line:
+        trie.insert(line)
+
+        
+
+
+
